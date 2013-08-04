858b4d2e62738946afe396a76eb550bddd7d6022
Joey Tuong <joey@tetrap.us>
2013-08-05 02:49:31 +1000

    Added flair, city guesser and gone*/nsfw stuff.

--- stalk.py
+++ stalk.py
@@ -5,7 +5,7 @@ import sys
 
 import praw
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
 	print("Usage: %s user" % sys.argv[0])
 	sys.exit(1)
 
@@ -28,14 +28,25 @@ ssubs = [i.subreddit.display_name for i in submissions]
 #print ssubs
 
 gonewild = open("data/gonewild.txt").read().split()
+gonemild = open("data/gonemild.txt").read().split()
 
 def is_gw_sub(subname):
 	sub = subname.lower()
 	if sub in gonewild:
 		return True
 	elif "gw" in sub or "gonewild" in sub:
-		print "Warning: heuristic detected subreddit not in list. Please report to developer."
+		print "Warning: heuristic detected subreddit not in list. Please report to the developer."
 		print "Click here: http://www.reddit.com/message/compose/?to=Lyucit&subject=Missing%20gw%20subreddit%20entry&message=" + sub + "%20is%20missing%20from%20your%20subreddit%20list."
+		return True
+	return False
+
+def is_gm_sub(subname):
+	sub = subname.lower()
+	if sub in gonemild:
+		return True
+	elif "gm" in sub or "gonemild" in sub:
+		print "Warning: heuristic detected subreddit not in list. Please report to the developer."
+		print "Click here: http://www.reddit.com/message/compose/?to=Lyucit&subject=Missing%20gm%20subreddit%20entry&message=" + sub + "%20is%20missing%20from%20your%20subreddit%20list."
 		return True
 	return False
 
@@ -43,6 +54,49 @@ def get_gonewild(posts):
 	posts = [i for i in posts if is_gw_sub(i.subreddit.display_name)]
 	return posts
 
-for i in get_gonewild(submissions):
-	print "%s (%s | %s up, %s down)" % (i.title, i.subreddit.url, i.ups, i.downs)
-	print "%s%s" % (i.url, " [NSFW]" if i.over_18 else "")
+def get_gonemild(posts):
+	posts = [i for i in posts if is_gm_sub(i.subreddit.display_name)]
+	return posts
+
+def get_nsfw_submissions(posts, include_gw=False):
+	posts = [i for i in posts if i.over_18 and not i.is_self]
+	if not include_gw:
+		posts = [i for i in posts if not is_gw_sub(i.subreddit.display_name)]
+	return posts
+
+gw = get_gonewild(submissions)
+if gw:
+	print "---- Gonewild Submissions ----"
+	for i in gw:
+		print "%s (%s | %s up, %s down)" % (i.title, i.subreddit.url, i.ups, i.downs)
+		print "%s%s" % (i.url, " [NSFW]" if i.over_18 else "")
+gm = get_gonemild(submissions)
+if gm:
+	print "---- Gonemild Submissions ----"
+	for i in gm:
+		print "%s (%s | %s up, %s down)" % (i.title, i.subreddit.url, i.ups, i.downs)
+		print "%s%s" % (i.url, " [NSFW]" if i.over_18 else "")
+nsfw = get_nsfw_submissions(submissions)
+if nsfw:
+	print "---- NSFW Submissions ----"
+	for i in nsfw:
+		print "%s (%s | %s up, %s down)" % (i.title, i.subreddit.url, i.ups, i.downs)
+		print "%s%s" % (i.url, " [NSFW]" if i.over_18 else "")
+
+subreddits = set(open("data/flairreddits.txt").read().split()) & ({i.lower() for i in ssubs} | {i.lower() for i in csubs})
+if "-f" in sys.argv:
+	subreddits |= {i.lower() for i in ssubs} | {i.lower() for i in csubs}
+
+flairs = {i:r.get_flair(i, sys.argv[1])["flair_text"] for i in subreddits}
+flairs = {k:v for k, v in flairs.items() if v}
+if flairs:
+	print "---- Known Flair ----"
+	for k, v in flairs.items():
+		print "r/%s - %s" % (k, v)
+
+cities = open("data/cities.txt").read().split()
+
+city = [i for i in csubs + ssubs if i.lower() in cities]
+if city:
+	print "Probably lives in %s" % max(city, key=city.count)
+
